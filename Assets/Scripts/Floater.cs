2bc34cd5c56d231de837631a8be916195cdca5ad
cemusta <nooneelse@gmail.com>
2016-10-14 02:48:41 +0300

    Initial commit
    finished tutorial

---
+++ Assets/Scripts/Floater.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floater : MonoBehaviour {
+
+	public float normalValue = 0.723f;
+	public float floatingParam = 0.15f;
+	
+	// Use this for initialization
+	void Start () {
+		normalValue = transform.position.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float sin = normalValue + floatingParam * Mathf.Sin (Time.time);
+
+		this.transform.position = new Vector3(transform.position.x,sin,transform.position.z);
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed = 10;
+	public Rigidbody rb;
+
+	public Text countText;
+	public GameObject winText;
+
+	private int countPickups = 0;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		countPickups = 0;
+		SetCountText ();
+		winText.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		Vector3 force = new Vector3 (x, 0f, z);
+
+		rb.AddForce(force * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag("pickup")) {
+			other.gameObject.SetActive (false);
+
+			countPickups++;
+			SetCountText ();
+
+			if (countPickups >= 11)
+				winText.SetActive (true);
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count :" + countPickups;
+	}
+}
